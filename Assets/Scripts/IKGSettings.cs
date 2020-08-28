a053ca9a4b0e086236718408b57fc102f15cff7e
Nicholas <nickc01@outlook.com>
2020-08-28 01:30:01 -0500

    Began work on WeaverCore Config Screen
    The screen is now interactable and shows basic settings on screen. It should also be able to save the settings to a file for later use.

---
+++ Assets/Scripts/IKGSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WeaverCore.Configuration;
+
+namespace Assets.Scripts
+{
+	public class IKGSettings : ModSettings
+	{
+		public bool hardMode = false;
+	}
+}
